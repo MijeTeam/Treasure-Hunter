93b7e777bc6de501986b333532e719ca82b12433
Rlaxogus123 <acekimcp@gmail.com>
2022-04-22 17:14:08 +0900

    PlayerController 이동 추가

--- TreasureHunter/Assets/PlayerController.cs
+++ TreasureHunter/Assets/PlayerController.cs
@@ -6,9 +6,38 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] QuadTreeManager qtMNG;
 
-    // Update is called once per frame
+    public float maxSpeed;
+    private Vector3 firstPos;
+    private SpriteRenderer spr_body;
+
+    public float fowardAngle;
+
+    private void Awake()
+    {
+        spr_body = GetComponent<SpriteRenderer>();
+
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        // Make Something ...
+    }
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = Input.mousePosition - firstPos;
+            Vector3 dir = pos.normalized;
+            fowardAngle = Mathf.Atan2(pos.y, pos.x);
+            transform.Translate(dir * Mathf.Clamp(pos.magnitude / 45, 0, maxSpeed) * Time.deltaTime);
+        }
+
         if (Input.GetKeyDown(KeyCode.X))
             qtMNG.GetTile();
     }
